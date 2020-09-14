e3c2667d2f59152d3f817b9afed2363774ae86fc
Maurici Armora <maurici.armora@gmail.com>
2020-09-14 16:18:54 +0200

    Devise setup and migration

--- db/schema.rb
+++ db/schema.rb
@@ -43,14 +43,16 @@ ActiveRecord::Schema.define(version: 2020_09_14_133345) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
-    t.string "password"
-    t.string "location"
     t.string "avatar"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "games"
