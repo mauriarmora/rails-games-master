a91f26d4cfa0421c2b5ba77d361d9095b4863a91
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2020-09-17 14:41:52 +0200

    added BCN to all seeds

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
