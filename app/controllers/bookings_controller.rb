b239804bbc6428c915797906072ce0147e3e1df2
Guillem Delas <guillemdelas@hotmail.com>
2020-09-17 17:00:14 +0200

    finished the dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,6 +18,24 @@ class BookingsController < ApplicationController
     end
   end
 
+  def accept
+    @booking = Booking.find(params[:booking_id])
+    @booking.status = "accepted"
+
+    @booking.save
+
+    redirect_to dashboard_path
+  end
+
+  def decline
+    @booking = Booking.find(params[:booking_id])
+    @booking.status = "declined"
+
+    @booking.save
+
+    redirect_to dashboard_path
+  end
+
   private
 
   def sanitize_params
