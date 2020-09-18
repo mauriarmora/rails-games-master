bc7378db88694199adbd753dde70836b459620af
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-09-18 12:31:31 +0200

    added a total price function to the calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -39,3 +39,4 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
 });
 
+
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,33 @@
 import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
+const startRange = document.querySelector("#range_start");
+const endRange = document.querySelector("#range_end");
+const priceNight = document.querySelector("#price-night");
+
+const computeTotalPrice = (event) => {
+  console.log("hi!");
+  const totalPriceTag = document.querySelector("#total-price");
+  const startDate = new Date(startRange.value.split(" to ")[0]);
+  const endDate = new Date(startRange.value.split(" to ")[1]);
+  const totalMiliseconds = endDate - startDate;
+  const totalInDays = totalMiliseconds / 86400000;
+  console.log(priceNight.value);
+  console.log(startDate, endDate);
+  const totalPrice = parseInt(priceNight.dataset.price, 10) * totalInDays;
+
+  totalPriceTag.innerHTML = totalPrice;
+  if (!Number.isNaN(totalPrice)) {
+  }
+};
+
 const initFlatpickr = () => {
   if (!document.querySelector("#range_start")) { return }
 
   flatpickr("#range_start", {
     altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
+    plugins: [new rangePlugin({ input: "#range_end"})],
+    onClose: computeTotalPrice
   });
 
 }
